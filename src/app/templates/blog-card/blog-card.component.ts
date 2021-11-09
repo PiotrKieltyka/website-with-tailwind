8262d9ffc437c37ca0c445ef7a9c532587caf0a7
Piotr Kieltyka <p.kieltyka@gmail.com>
2021-11-09 20:03:26 +0100

    flat components

--- src/app/templates/blog-card/blog-card.component.ts
+++ src/app/templates/blog-card/blog-card.component.ts
@@ -2,8 +2,22 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'site-blog-card',
-  templateUrl: './blog-card.component.html',
-  styleUrls: ['./blog-card.component.scss'],
+  template: `
+    <ng-container>
+      <div class='w-full h-full flex flex-col items-center justify-center text-center'>
+        <h3 class='font-medium text-gray-700'>{{title}}</h3>
+        <h5 class='text-xs font-light my-4 italic'>{{date}}</h5>
+        <span class='font-normal max-w-4xl' [innerHTML]='content'></span>
+        <ng-template [ngIf]='link'>
+          <a
+            class='text-sm font-light w-3/4 md:w-1/4 h-8 my-4 mt-4 grid items-center justify-center bg-white hover:shadow-lg hover:bg-gray-700 hover:text-white transition-all shadow-inner rounded-lg'
+            href='{{link}}'
+          >Learn more</a>
+        </ng-template>
+      </div>
+    </ng-container>
+  `,
+  styles: [],
 })
 export class BlogCardComponent {
   @Input() id?: string = '';
--- src/app/templates/nav-menu/nav-menu.component.ts
+++ src/app/templates/nav-menu/nav-menu.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { NavMenuInterface } from '../../models/NavMenu.interface';
 import { NavMenuModel } from '../../models/NavMenu.model';
 
 @Component({
   selector: 'site-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.scss'],
+  template: `
+    <nav class='flex flex-row items-center justify-between'>
+      <ng-container *ngFor='let navItem of navigation'>
+        <a class='px-1 md:px-8 font-light text-sm cursor-pointer hover:bg-gray-200 rounded-xl px-4 py-2'
+           *ngIf='(navItem.protected === false) || (navItem.protected && isLoggedIn)'
+           routerLink='{{navItem.route}}' routerLinkActive='active'
+        ><i class='{{navItem.icon}} mr-2 hidden sm:inline'></i>
+          {{navItem.text}}
+        </a>
+      </ng-container>
+    </nav>
+  `,
+  styles: [`
+    .active {
+      @apply border-b-2 rounded-b-none;
+    }
+  `]
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnDestroy {
   isLoggedIn: boolean = false;
   navigation: Array<NavMenuInterface> = NavMenuModel;
 
@@ -16,5 +31,9 @@ export class NavMenuComponent {
     this.authService.isLoggedIn.subscribe(
       (value: boolean) => (this.isLoggedIn = value),
     );
+  }
+
+  ngOnDestroy() {
+    this.authService.isLoggedIn.unsubscribe();
   }
 }
