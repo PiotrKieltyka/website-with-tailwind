e242913ca0fbae3e82fb1fd152235219153cd997
Piotr Kieltyka <p.kieltyka@gmail.com>
2022-01-29 10:43:56 +0100

    add animation to blog button

--- src/app/templates/link-follow-copy/link-follow-copy.component.ts
+++ src/app/templates/link-follow-copy/link-follow-copy.component.ts
@@ -3,9 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'site-link-follow-copy',
   template: `
-    <div class='text-xs font-light w-3/4 md:w-1/4 h-8 my-4 mt-4 flex items-center justify-evenly bg-white hover:shadow-xs hover:shadow-indigo-300 hover:bg-gradient-to-r hover:from-indigo-500 hover:to-emerald-400 hover:text-white hover:font-normal transition-all shadow-inner shadow-indigo-300 rounded-lg'>
+    <div class='text-xs font-light w-3/4 md:w-1/4 h-8 my-4 mt-4 flex items-center justify-evenly bg-white hover:shadow-xs hover:shadow-indigo-300 hover:bg-gradient-to-r hover:from-indigo-500 hover:to-emerald-400 hover:text-white hover:font-normal transition-all shadow-inner shadow-indigo-300 rounded-lg'
+      data-aos='flip-up' data-aos-duration='300' data-aos-delay='650'>
       <a href='{{ url }}' target='_blank' rel='noopener noreferrer'>Go to</a>
-      <span (click)='copyUrlToClipboard()' class='cursor-copy'>Copy</span>
+      <span (click)='copyUrlToClipboard()' class='cursor-copy active:text-pink-400'>Copy</span>
     </div>
   `,
   styles: []
@@ -14,9 +15,6 @@ export class LinkFollowCopyComponent {
   @Input() url: string = '';
 
   copyUrlToClipboard() {
-    navigator.clipboard.writeText(this.url)
-      .then(() => {
-        console.log('URL copied to clipboard');
-      });
+    navigator.clipboard.writeText(this.url);
   }
 }
